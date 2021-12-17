58628c90ad3b01fdb72d96ee99e2d615aec6fb17
jaydeep swami <jaydeep.swami@searce.com>
2021-12-17 11:24:29 +0530

    updated code

--- gcs/main.tf
+++ gcs/main.tf
@@ -2,7 +2,7 @@
 	GCS Bucket configuration for Terraform State management
  *****************************************/
 module "gcs" {
-  source            = "../modules/cloud_storage"
+  source            = "../cloud_storage"
   project_id        = var.project_id
   location          = var.region
   name              = "jay-test-bucket"
